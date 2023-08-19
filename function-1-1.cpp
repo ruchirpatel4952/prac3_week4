dcd67446b962599dca9d4c820c275ef719649643
Ruchir <ruchirpatel2000@gmail.com>
2023-08-19 12:48:34 +0930

    first commit

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,7 +4,7 @@ Person* createPersonArray(int n) {
     Person* persons = new Person[n];
     
     for (int i = 0; i < n; ++i) {
-        persons[i].name = "John Doe";
+        persons[i].name = std::string("John Doe");
         persons[i].age = 0;
     }
     
